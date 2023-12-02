5dbce3748c46719d090830fcbec78ea70123bdee
AvarbyKira <1248488996@qq.com>
2023-12-02 13:57:21 +0800

    initialization

---
+++ index.js
@@ -0,0 +1,16 @@
+// Link: https://www.youtube.com/watch?v=bMknfKXIFA8
+
+// Hello World
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+
+// Learning Function
+function info() {
+    return (
+        <div>
+            <h1>I am learning React!</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(info(), document.getElementById('root'));
+
